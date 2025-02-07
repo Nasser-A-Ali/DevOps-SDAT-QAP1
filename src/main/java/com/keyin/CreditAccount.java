f13eacb4759ee06897734523abc1c8da715d0c73
Nasser Ali <nasserali60@live.com>
2025-02-07 17:58:24 -0330

    feat: create BankAccountService class and some demo cases in Main

--- src/main/java/com/keyin/CreditAccount.java
+++ src/main/java/com/keyin/CreditAccount.java
@@ -3,16 +3,19 @@ package com.keyin;
 public class CreditAccount extends BankAccount {
     private static final double INTEREST_RATE = 0.1999;
 
-    private double creditLimit;
+    private double creditLimit = 1000.00;
 
     public CreditAccount(String accountHolderName, double initialBalance, double creditLimit) {
         super(accountHolderName, initialBalance);
         this.creditLimit = creditLimit;
     }
 
-    public CreditAccount(String accountHolderName, double creditLimit) {
+    public CreditAccount(String accountHolderName, double initialBalance) {
+        super(accountHolderName, initialBalance);
+    }
+
+    public CreditAccount(String accountHolderName){
         super(accountHolderName);
-        this.creditLimit = creditLimit;
     }
 
     @Override
